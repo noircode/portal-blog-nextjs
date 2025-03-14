d196e1eed7afcb5050de64cba5c72e19a9c64778
Kang Noir <simaul.noir@gmail.com>
2025-03-14 23:43:20 +0700

    add dashboard

--- src/app/(auth)/login/form/index.tsx
+++ src/app/(auth)/login/form/index.tsx
@@ -3,9 +3,54 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { useRouter } from "next/navigation"
+import { useState } from "react";
+import { useFormStatus } from "react-dom";
+import { formSchema } from "./validation";
+import axiosInstance from "../../../../../lib/axios";
+import { setCookie } from "cookies-next";
 
+
+
+const SubmitButton = () => {
+    const { pending } = useFormStatus();
+
+    return (
+        <Button disabled={pending} className="w-full cursor-pointer" type="submit">
+            {pending ? 'Loading...' : 'Submit'}
+        </Button>
+    )
+}
 const FormSignIn = () => {
     const router = useRouter();
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState<string[]>([]);
+
+    const handleLogin = async (e: React.FormEvent) => {
+        e.preventDefault();
+        setError([]);
+
+        const validation = formSchema.safeParse({
+            email,
+            password,
+        })
+
+        if (!validation.success) {
+            setError(validation.error.errors.map((error) => error.message));
+            return;
+        }
+
+        await axiosInstance.post('/login', {email: email, password: password})
+            .then((response) => {
+                setCookie('accessToken', response.data.access_token);
+                router.push('/dashboard');
+            })
+            .catch((error) => {
+                setError([`Login gagal. Periksa email dan password anda`])
+                return;
+            })            
+    }
 
     return (
         <div className="w-full h-screen">
@@ -16,12 +61,24 @@ const FormSignIn = () => {
                     </h2>
                 </div>
 
-                <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-                    <form action="#" className="space-y-6">
-                        <Input type="email" placeholder="Email address" name="email" required />
-                        <Input type="password" placeholder="Password" name="password" required />
+                {error.length > 0 && (
+                    <div className="mx-auto my-7 bg-red-500 w-[400px] p-4 rounded-lg text-white">
+                        <div className="font-bold mb-4"> Error Message</div>
 
-                        <Button>Submit</Button>
+                        <ul className="list-disc list-inside">
+                            {error?.map((message, index) => (
+                                <li key={index}>{message}</li>
+                            ))}
+                        </ul>
+                    </div>
+                )}
+
+                <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                    <form onSubmit={handleLogin} className="space-y-6">
+                        <Input type="email" onChange={(e) => setEmail(e.target.value)} value={email} placeholder="Email address" name="email" required />
+                        <Input type="password" onChange={(e) => setPassword(e.target.value)} value={password} placeholder="Password" name="password" required />
+
+                        <SubmitButton />
                     </form>
                 </div>
             </div>
---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,88 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono, Inter } from "next/font/google";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { BookOpen, Layers3, User } from "lucide-react";
+import ButtonLogout from "./[home]/components/ButtonLogout";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+const inter = Inter({
+  variable: "--font-inter",
+  subsets: ["latin"],
+})
+
+export const metadata: Metadata = {
+  title: "Dashboard",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={inter.className}
+      >
+        <section>
+          <nav className="border-b border-muted p-5">
+            <div className="flex flex-row items-center justify-between">
+              <span className="font-bold text-primary">
+                Portal News
+              </span>
+            </div>
+          </nav>
+
+          <section className="flex flex-row gap-5 items-start flex-nowrap">
+            <section className="grow-0 w-[20%] h-screen shadow p-5 space-y-5">
+              <div className="space-y-2">
+                <Button variant={"ghost"} asChild className="w-full justify-start">
+                  <Link href={"/dashboard"}>Dashboard</Link>
+                </Button>
+              </div>
+
+              <div className="space-y-2">
+                <div className="uppercase text-xs font-bold">
+                  Master Data
+                </div>
+                <Button variant={"ghost"} asChild className="w-full justify-start">
+                  <Link href={""}>
+                    <Layers3 className="mr-2 w-4 h4" />
+                    Kategori
+                  </Link>
+                </Button>
+                <Button variant={"ghost"} asChild className="w-full justify-start">
+                  <Link href={""}>
+                    <BookOpen className="mr-2 w-4 h4" />
+                    Konten
+                  </Link>
+                </Button>
+                <Button variant={"ghost"} asChild className="w-full justify-start">
+                  <Link href={""}>
+                    <User className="mr-2 w-4 h4" />
+                    Pengguna
+                  </Link>
+                </Button>
+              </div>
+              <ButtonLogout />
+            </section>
+
+            <section className="grow mr-5 mt-5 h-[87vh] overflow-y-auto">
+              {children}
+            </section>
+          </section>
+        </section>
+      </body>
+    </html>
+  );
+}
